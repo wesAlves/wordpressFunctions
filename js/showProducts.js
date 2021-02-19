8378d6e59eddec6c8183b603917bb051d1ece638
Wesley <wes.bnvnd@gmail.com>
2021-02-19 13:31:55 -0300

    wip: is null are avoid, need to manage priveta session

--- js/showProducts.js
+++ js/showProducts.js
@@ -1,49 +1,49 @@
 const showProductsList = (produtoCategorias, divName, page) => {
-  // const divProd = document.getElementById(divName);
-  // divProd.innerHTML = "";
+    // const divProd = document.getElementById(divName);
+    // divProd.innerHTML = "";
 
-  const paginationDisplay = document.querySelector(
-    `.pagination_show_${divName}`
-  );
+    const paginationDisplay = document.querySelector(
+        `.pagination_show_${divName}`
+    );
 
-  const qntProductCategoryPagination = JSON.parse(
-    localStorage.getItem(`@PontoDoMalte:${divName}-totalQauntitie`)
-  );
-  if (paginationDisplay !== null) {
-    paginationDisplay.innerHTML = `Monstrando grupo ${page} de ${Math.ceil(
-      qntProductCategoryPagination / 16
-    )}`;
-  }
+    const qntProductCategoryPagination = JSON.parse(
+        localStorage.getItem(`@PontoDoMalte:${divName}-totalQauntitie`)
+    );
+    if (paginationDisplay !== null) {
+        paginationDisplay.innerHTML = `Monstrando grupo ${page} de ${Math.ceil(
+            qntProductCategoryPagination / 16
+        )}`;
+    }
 
-  const getProdutoData = JSON.parse(
-    localStorage.getItem(`@PontoDoMalte:${produtoCategorias}-page${page}`)
-  );
+    const getProdutoData = JSON.parse(
+        localStorage.getItem(`@PontoDoMalte:${produtoCategorias}-page${page}`)
+    );
 
-  getProdutoData.map((produtoCategoria) => {
-    const produtoCategoriaOBJ = {
-      nome: `${produtoCategoria.title.rendered}`,
-      preco_01: `${produtoCategoria.acf.preco_01}`,
-      preco_02: `${produtoCategoria.acf.preco_2}`,
-      preco_03: `${produtoCategoria.acf.preco_3}`,
-      tipo_preco_01: `${produtoCategoria.acf.tipo_preco_1}`,
-      tipo_preco_02: `${produtoCategoria.acf.tipo_preco_2}`,
-      tipo_preco_03: `${produtoCategoria.acf.tipo_preco_2}`,
-      potencial_malte: `${produtoCategoria.acf.potencial_malte}`,
-      cor_malte: `${produtoCategoria.acf.cor_malte}`,
-      tipo_cor_malte: `${produtoCategoria.acf.tipo_cor_malte_malte}`,
-      tipo_lupulo: `${produtoCategoria.acf.tipo_lupulo}`,
-      aplicacao_lupulo: `${produtoCategoria.acf.aplicacao_lupulo}`,
-      pais_de_origem: `${produtoCategoria.acf.pais_de_origem}`,
-      alphas: `${produtoCategoria.acf.alphas}`,
-      oleos: `${produtoCategoria.acf.oleos}`,
-      embalagem: `${produtoCategoria.acf.embalagem}`,
-      tipo_de_fermentacao: `${produtoCategoria.acf.tipo_de_fermentacao}`,
-      descricao: `${produtoCategoria.acf.descricao}`,
-      foto: `${produtoCategoria.acf.foto}`,
-      link: `${produtoCategoria.link}`,
-    };
+    getProdutoData.map((produtoCategoria) => {
+        const produtoCategoriaOBJ = {
+            nome: `${produtoCategoria.title.rendered}`,
+            preco_01: `${produtoCategoria.acf.preco_01}`,
+            preco_02: `${produtoCategoria.acf.preco_2}`,
+            preco_03: `${produtoCategoria.acf.preco_3}`,
+            tipo_preco_01: `${produtoCategoria.acf.tipo_preco_1}`,
+            tipo_preco_02: `${produtoCategoria.acf.tipo_preco_2}`,
+            tipo_preco_03: `${produtoCategoria.acf.tipo_preco_2}`,
+            potencial_malte: `${produtoCategoria.acf.potencial_malte}`,
+            cor_malte: `${produtoCategoria.acf.cor_malte}`,
+            tipo_cor_malte: `${produtoCategoria.acf.tipo_cor_malte_malte}`,
+            tipo_lupulo: `${produtoCategoria.acf.tipo_lupulo}`,
+            aplicacao_lupulo: `${produtoCategoria.acf.aplicacao_lupulo}`,
+            pais_de_origem: `${produtoCategoria.acf.pais_de_origem}`,
+            alphas: `${produtoCategoria.acf.alphas}`,
+            oleos: `${produtoCategoria.acf.oleos}`,
+            embalagem: `${produtoCategoria.acf.embalagem}`,
+            tipo_de_fermentacao: `${produtoCategoria.acf.tipo_de_fermentacao}`,
+            descricao: `${produtoCategoria.acf.descricao}`,
+            foto: `${produtoCategoria.acf.foto}`,
+            link: `${produtoCategoria.link}`,
+        };
 
-    createCardType(divName, produtoCategoriaOBJ);
-    // console.log(produtoCategoriaOBJ.link);
-  });
+        createCardType(divName, produtoCategoriaOBJ);
+        // console.log(produtoCategoriaOBJ.link);
+    });
 };
