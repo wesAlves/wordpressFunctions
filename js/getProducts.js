f0ee6232466d112fb63eaef4b3f2427483bfa551
Wesley Benvindo <wes.bnvnd@gmail.com>
2021-02-20 00:28:58 -0300

    wip: css products done!

--- js/getProducts.js
+++ js/getProducts.js
@@ -1,83 +1,93 @@
+const sessitionData = [];
 (async () => {
-  const requestCategories = new Request(
-    "http://pontodomalte.com.br/wp-json/wp/v2/categories?parent=27",
-    {
-      method: "GET",
-    }
-  );
-
-  const categoriesNameForMenu = [];
-  const categoriesSlugForMenu = [];
-
-  const getCategories = await fetch(requestCategories);
-
-  const categoriesList = await getCategories.json();
-
-  await categoriesList.map((category) => {
-    categoriesNameForMenu.push(category.name);
-    categoriesSlugForMenu.push(category.slug);
-
-    const requestProducts = new Request(
-      `http://pontodomalte.com.br/wp-json/wp/v2/produtos?categories=${category.id}&_embed&per_page=16
-      `,
-      {
-        method: "GET",
-      }
+    const requestCategories = new Request(
+        "http://pontodomalte.com.br/wp-json/wp/v2/categories?parent=27",
+        {
+            method: "GET",
+        }
     );
 
-    (async () => {
-      const getProductsByCategory = await fetch(requestProducts);
+    const categoriesNameForMenu = [];
+    const categoriesSlugForMenu = [];
 
-      const productTotalQauntitie = getProductsByCategory.headers.get(
-        "x-wp-total"
-      );
+    const getCategories = await fetch(requestCategories);
 
-      //TODO SET A TIME FOR RELOAD IF LOCAL IS NOT THE SAME AS REQUEST
-      // localStorage.getItem(`@PontoDoMalte:${category.slug}-totalQauntitie`) ===
-      // JSON.stringify(productTotalQauntitie)
-      //   ? null
-      //   : setTimeout(location.reload(), 10000);
+    const categoriesList = await getCategories.json();
 
-      localStorage.setItem(
-        `@PontoDoMalte:${category.slug}-totalQauntitie`,
-        JSON.stringify(productTotalQauntitie)
-      );
+    await categoriesList.map((category) => {
+        categoriesNameForMenu.push(category.name);
+        categoriesSlugForMenu.push(category.slug);
 
-      if (productTotalQauntitie / 16 >= 1) {
-        const totalPages = productTotalQauntitie / 16;
-
-        const pageNumber = Math.ceil(totalPages);
-
-        for (let i = 1; i <= pageNumber; i++) {
-          const currentPage = i;
-
-          (async () => {
-            const getProductsByCategoryPagination = await fetch(
-              `http://pontodomalte.com.br/wp-json/wp/v2/produtos?categories=${category.id}&_embed&per_page=16&page=${currentPage}`
-            );
-
-            const productsResponse = await getProductsByCategoryPagination.json();
-
-            localStorage.setItem(
-              `@PontoDoMalte:${category.slug}-page${currentPage}`,
-              JSON.stringify(productsResponse)
-            );
-          })();
-        }
-        return;
-      } else {
-        const productsResponse = await getProductsByCategory.json();
-
-        localStorage.setItem(
-          `@PontoDoMalte:${category.slug}-page1`,
-          JSON.stringify(productsResponse)
+        const requestProducts = new Request(
+            `http://pontodomalte.com.br/wp-json/wp/v2/produtos?categories=${category.id}&_embed&per_page=16
+        `,
+            {
+                method: "GET",
+            }
         );
-      }
-      return;
-    })();
-    // callMe(category.slug, 1);
-    createDivProdutc(category.slug, 1);
-  });
 
-  createMenuProducts(categoriesNameForMenu, categoriesSlugForMenu);
+        (async () => {
+            const getProductsByCategory = await fetch(requestProducts);
+
+            const productTotalQauntitie = getProductsByCategory.headers.get(
+                "x-wp-total"
+            );
+
+            sessitionData.push({
+                category: `${category.slug}`,
+                quantityItems: `${productTotalQauntitie}`,
+            });
+
+            console.log(productTotalQauntitie);
+            return;
+
+            //TODO SET A TIME FOR RELOAD IF LOCAL IS NOT THE SAME AS REQUEST
+            // localStorage.getItem(`@PontoDoMalte:${category.slug}-totalQauntitie`) ===
+            // JSON.stringify(productTotalQauntitie)
+            //   ? null
+            //   : setTimeout(location.reload(), 10000);
+
+            //localStorage.setItem(
+            //    `@PontoDoMalte:${category.slug}-totalQauntitie`,
+            //    JSON.stringify(productTotalQauntitie)
+            //);
+
+            if (productTotalQauntitie / 16 >= 1) {
+                const totalPages = productTotalQauntitie / 16;
+
+                const pageNumber = Math.ceil(totalPages);
+
+                for (let i = 1; i <= pageNumber; i++) {
+                    const currentPage = i;
+
+                    (async () => {
+                        const getProductsByCategoryPagination = await fetch(
+                            `http://pontodomalte.com.br/wp-json/wp/v2/produtos?categories=${category.id}&_embed&per_page=16&page=${currentPage}`
+                        );
+
+                        const productsResponse = await getProductsByCategoryPagination.json();
+
+                        /*localStorage.setItem(
+                            `@PontoDoMalte:${category.slug}-page${currentPage}`,
+                            JSON.stringify(productsResponse)
+                        );*/
+                    })();
+                }
+                return;
+            } else {
+                const productsResponse = await getProductsByCategory.json();
+
+                /* localStorage.setItem(
+                    `@PontoDoMalte:${category.slug}-page1`,
+                    JSON.stringify(productsResponse)
+                );*/
+            }
+            return;
+        })();
+        // callMe(category.slug, 1);
+        createDivProdutc(category.slug, 1);
+    });
+    console.log(sessitionData);
+
+    createMenuProducts(categoriesNameForMenu, categoriesSlugForMenu);
 })();
