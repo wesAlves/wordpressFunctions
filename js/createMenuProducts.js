50a6c3bf39e25a2d8cbd070a4f7c8b41ba05cc96
Wesley Benvindo <wes.bnvnd@gmail.com>
2021-02-21 14:13:53 -0300

    destaque is ripped off

--- js/createMenuProducts.js
+++ js/createMenuProducts.js
@@ -2,48 +2,50 @@ const createMenuProducts = (categoriesNameForMenu, categoriesSlugForMenu) => {
   const menuUL = document.getElementById("menuProdutos");
 
   categoriesNameForMenu.map((categoria, index) => {
-    const hideShowElement = document.getElementById(
-      `containerProdutos_${categoriesSlugForMenu[index]}`
-    );
+    if (categoria !== "Destaque") {
+      const hideShowElement = document.getElementById(
+        `containerProdutos_${categoriesSlugForMenu[index]}`
+      );
 
-    const titlePlace = document.getElementById(
-      `containerProdutos_${categoriesSlugForMenu[index]}`
-    );
+      const titlePlace = document.getElementById(
+        `containerProdutos_${categoriesSlugForMenu[index]}`
+      );
 
-    const categoryTitle = document.createElement("H2");
-    const menuItem = document.createElement("LI");
-    const categoryMenuItem = document.createElement("INPUT");
-    const categoryMenuItemLabel = document.createElement("LABEL");
-    const label = document.createTextNode(` ${categoria}`);
+      const categoryTitle = document.createElement("H2");
+      const menuItem = document.createElement("LI");
+      const categoryMenuItem = document.createElement("INPUT");
+      const categoryMenuItemLabel = document.createElement("LABEL");
+      const label = document.createTextNode(` ${categoria}`);
 
-    categoryTitle.setAttribute("class", "categoryTitle");
+      categoryTitle.setAttribute("class", "categoryTitle");
 
-    menuItem.setAttribute("class", "selectMenu");
+      menuItem.setAttribute("class", "selectMenu");
 
-    categoryMenuItem.setAttribute("type", "radio");
-    categoryMenuItem.setAttribute("id", categoria);
-    categoryMenuItem.setAttribute("value", categoriesSlugForMenu[index]);
-    categoryMenuItem.setAttribute("name", "categoriaMenu");
+      categoryMenuItem.setAttribute("type", "radio");
+      categoryMenuItem.setAttribute("id", categoria);
+      categoryMenuItem.setAttribute("value", categoriesSlugForMenu[index]);
+      categoryMenuItem.setAttribute("name", "categoriaMenu");
 
-    categoryMenuItemLabel.setAttribute("for", categoria);
+      categoryMenuItemLabel.setAttribute("for", categoria);
 
-    categoryTitle.innerHTML = categoria;
+      categoryTitle.innerHTML = categoria;
 
-    if (titlePlace !== null) {
-      titlePlace.insertBefore(categoryTitle, titlePlace.firstChild);
-    }
+      if (titlePlace !== null) {
+        titlePlace.insertBefore(categoryTitle, titlePlace.firstChild);
+      }
 
-    categoryMenuItemLabel.appendChild(label);
+      categoryMenuItemLabel.appendChild(label);
 
-    menuItem.appendChild(categoryMenuItem);
-    menuItem.appendChild(categoryMenuItemLabel);
+      menuItem.appendChild(categoryMenuItem);
+      menuItem.appendChild(categoryMenuItemLabel);
 
-    menuUL.appendChild(menuItem);
+      menuUL.appendChild(menuItem);
 
-    if (categoria !== null) {
-      document.getElementById(categoria).onclick = function () {
-        toggleHidden(categoria, categoriesSlugForMenu[index]);
-      };
+      if (categoria !== null) {
+        document.getElementById(categoria).onclick = function () {
+          toggleHidden(categoria, categoriesSlugForMenu[index]);
+        };
+      }
     }
   });
 };
